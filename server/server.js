e4fd6e94ca45c024bf36d666c3a38a56a12ec0bc
Kevin Shin <kevinwshin@utexas.edu>
2019-05-30 06:31:10 -0500

    "debugging"

--- server/server.js
+++ server/server.js
@@ -8,10 +8,14 @@ let server;
 let users = [];
 const grid = [];
 
+console.log("1");
+
 const startBeat = function(duration){
     io.emit("startBeat");
     setTimeout(startBeat, duration);
 };
+
+console.log("2");
 
 //starts the server, gets the port from heroku if possible
 // const server = http.createServer(function(request, response) {
@@ -24,16 +28,28 @@ const setup = function() {
         grid.push(Array(HEIGHT).fill(false));
     }
 
+    console.log("4");
+
     //starts the server, gets the port from heroku if possible
     server = http.createServer();
+
+    console.log("5");
+
     server.listen(process.env.PORT || 8080);
+
+    console.log("6");
 
     //setup event handlers
     io = require("socket.io")(server);
+
+    console.log("7");
+
     io.on("connection", function(socket) {
         //register users and send state on connect
         users.push(socket);
         socket.emit("setup", grid);
+
+        console.log("8");
 
         //deregister users on disconnect
         socket.on("disconnect", function() {
@@ -45,19 +61,30 @@ const setup = function() {
             }
         });
 
+        console.log("9");
+
         //set true and broadcast on activation
         socket.on("activation", function(cell) {
             grid[cell[0]][cell[1]] = true;
             socket.broadcast.emit("activation", cell);
         });
 
+        console.log("10");
+
         //set false and broadcast on deactivation
         socket.on("deactivation", function(cell) {
             grid[cell[0]][cell[1]] = false;
             socket.broadcast.emit("deactivation", cell);
         });
+
+        console.log("12");
     });
+
+    console.log("12");
 
     startBeat();
 };
+
+console.log("3");
+
 setup();
